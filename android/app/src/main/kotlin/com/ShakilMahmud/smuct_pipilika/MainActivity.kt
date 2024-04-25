aeab706fd8db9e4a07b93135aad76c75ac0c2327
s4k1l <69shakilmahmud@gmail.com>
2024-04-26 00:46:15 +0600

    Pipilika Webview Done

---
+++ android/app/src/main/kotlin/com/ShakilMahmud/smuct_pipilika/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ShakilMahmud.smuct_pipilika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://smuct.pipilikasoft.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
